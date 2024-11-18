0f79c066d1ac2a59375d5c925a41700c2d80685d
Franck Jiang <franck.jiang@outlook.fr>
2024-11-18 11:28:36 +0100

    adding add/remove worker user + front button mapping

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ from auth import verify_autorization_header, generate_access_token, get_current_
 from schemas.user import User as model_user
 from routers.animal import router_animal
 from routers.user import router_user
+from starlette.middleware.sessions import SessionMiddleware
+
 
 
 templates = Jinja2Templates(directory="templates")
@@ -21,12 +23,24 @@ app = FastAPI(
     version="0.0.1",
 )
 
+app.add_middleware(SessionMiddleware, secret_key="SecretKey")
 app.include_router(router_animal)
 app.include_router(router_user)
 
 @app.get("/")
 def read_root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    try :
+        user = request.session["user"]
+        role = request.session["role"]
+    except:
+        user = None
+        role = None
+    return templates.TemplateResponse("home.html", {"request": request, 
+                                                    "user": user, 
+                                                    "role": role})
+
+def is_logged_in(request: Request):
+    return request.session.get("user") is not None
 
 @app.get("/add_animal")
 async def add_animal_form(request: Request):
@@ -43,15 +57,18 @@ def query(request: Request, query: str):
     session.flush()
     
 @app.get("/role")
-def get_role(request: Request, user: User = Depends(get_current_user)):
-    print(f"{user=}")
-    _, role = user
-    if role == 1 :
-        return templates.TemplateResponse("admin.html", {"request": request})
-    elif role == 2 :
-        return templates.TemplateResponse("user.html", {"request": request})
-    elif role == 3 :
-        return templates.TemplateResponse("worker.html", {"request": request})
-    else:
-        return templates.TemplateResponse("welcome.html", {"request": request})
+def get_role(request: Request):
+    user_logged_in = is_logged_in(request)
+    print(f"{user_logged_in=}")
+    return templates.TemplateResponse("index.html", {"request": request, "user_logged_in": user_logged_in})
+
+    # _, role = user
+    # if role == 1 :
+    #     return templates.TemplateResponse("admin.html", {"request": request})
+    # elif role == 2 :
+    #     return templates.TemplateResponse("user.html", {"request": request})
+    # elif role == 3 :
+    #     return templates.TemplateResponse("worker.html", {"request": request})
+    # else:
+    #     return templates.TemplateResponse("welcome.html", {"request": request})
     
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,5 @@
 import hashlib
-from fastapi import APIRouter, Depends, Request, Form
+from fastapi import APIRouter, Depends, Request, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 
@@ -16,9 +16,6 @@ router_user = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-def welcome(request: Request, access_token: str):
-    return templates.TemplateResponse("welcome.html", {"request": request, "access_token": access_token})
-
 @router_user.get("/signup")
 async def sign_up(request: Request):
     print("HTML lololol")
@@ -30,11 +27,19 @@ def signup(request: Request,
            db: Session = Depends(get_db)):
     
     print(f"Username: {user.username}, Password: {user.password}")
-    new_user = create_user(db=db, user=User(username=user.username, password=user.password))
+    try :
+        new_user = create_user(db=db, user=User(username=user.username, password=user.password))
+    except HTTPException as e:
+        print(f"Error while creating user: {e.detail}")
+        raise HTTPException(status_code=e.status_code, detail=f"User creation failed: {e.detail}")
+
     access_token = generate_access_token(db=db, 
                                         user=new_user)
-    
-    return welcome(request=request, access_token=access_token)
+    request.session["user"] = new_user.username
+    request.session["role"] = new_user.category_id
+    return templates.TemplateResponse("welcome.html", {"request": request, 
+                                                       "access_token": access_token, 
+                                                       "user": new_user.username})
 
 
 @router_user.get("/signin")
@@ -50,7 +55,80 @@ def signin(request: Request,
     hashed = hashlib.sha256(password.encode()).hexdigest()
     query = select(User).where(User.username == username, User.password == hashed)
     res = db.execute(query).scalars().first()  
-    print(res)
-    access_token = generate_access_token(db=db, 
-                                         user=res)
-    return welcome(request=request, access_token=access_token)
+    if res:
+        print(res)
+        request.session["user"] = res.username
+        request.session["role"] = res.category_id
+        access_token = generate_access_token(db=db, 
+                                            user=res)
+        return templates.TemplateResponse("welcome.html", {"request": request, 
+                                                           "access_token": access_token, 
+                                                           "user": res.username})
+    else:
+        return templates.TemplateResponse("signin.html", {"request": request, "error_message": "Invalid username or password"})
+    
+
+@router_user.get("/logout")
+def logout(request: Request):
+    request.session.clear() 
+    return RedirectResponse(url="/", status_code=303)
+
+@router_user.get("/add_worker")
+def add_worker(request: Request, db: Session = Depends(get_db)):
+    users = db.query(User).filter(User.category_id==2).all()
+    api_action = "/add_worker"
+    return templates.TemplateResponse("add_worker.html", {"request": request, "users": users, "api_action": api_action})
+    
+@router_user.post("/add_worker")
+def add_worker_post(request: Request, user_id: int = Form(...), db: Session = Depends(get_db)):
+    # Find the user based on the user_id from the form
+    selected_user = db.query(User).filter(User.id==user_id).first()
+    if selected_user:
+        if selected_user.category_id == 2:
+            selected_user.category_id = 3
+            db.commit()
+            return {"message": f"Selected worker: {selected_user.username}, ID: {selected_user.id}, category_id changed to {selected_user.category_id}"}
+        else:
+            return {"error": "User's category_id is not 'User', so it cannot be updated to 'Worker'"}
+    else:
+        return {"error": "User not found"}
+    
+@router_user.get("/remove_worker")
+def add_worker(request: Request, db: Session = Depends(get_db)):
+    users = db.query(User).filter(User.category_id==3).all()
+    api_action = "/remove_worker"
+    return templates.TemplateResponse("add_worker.html", {"request": request, "users": users, "api_action": api_action})
+
+@router_user.post("/remove_worker")
+def add_worker_post(request: Request, user_id: int = Form(...), db: Session = Depends(get_db)):
+    # Find the user based on the user_id from the form
+    selected_user = db.query(User).filter(User.id==user_id).first()
+    if selected_user:
+        if selected_user.category_id == 3:
+            selected_user.category_id = 2
+            db.commit()
+            return {"message": f"Selected worker: {selected_user.username}, ID: {selected_user.id}, category_id changed to {selected_user.category_id}"}
+        else:
+            return {"error": "User's category_id is not 'Worker', so it cannot be updated to 'User'"}
+    else:
+        return {"error": "User not found"}
+    
+@router_user.get("/remove_user")
+def add_worker(request: Request, db: Session = Depends(get_db)):
+    users = db.query(User).filter(User.category_id==2).all()
+    api_action = "/remove_user"
+    return templates.TemplateResponse("add_worker.html", {"request": request, "users": users, "api_action": api_action})
+
+@router_user.post("/remove_user")
+def add_worker_post(request: Request, user_id: int = Form(...), db: Session = Depends(get_db)):
+    # Find the user based on the user_id from the form
+    selected_user = db.query(User).filter(User.id==user_id).first()
+    if selected_user:
+        if selected_user.category_id == 2:
+            db.delete(selected_user)
+            db.commit()
+            return {"message": f"Selected worker: {selected_user.username}, ID: {selected_user.id}, removed"}
+        else:
+            return {"error": "User's category_id is not 'User', so it cannot be removed"}
+    else:
+        return {"error": "User not found"}
